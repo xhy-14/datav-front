e1722f97beaedbe0e6acb47aae6457f69a1c194b
xhy-14 <74343154+xhy-14@users.noreply.github.com>
2024-01-23 15:00:29 +0800

    修改登录样式

--- src/views/workplace/DataEditor.vue
+++ src/views/workplace/DataEditor.vue
@@ -87,7 +87,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .box {
   width: 100%;
   height: 100%;
--- src/views/workplace/index.vue
+++ src/views/workplace/index.vue
@@ -60,7 +60,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
   height: 100%;
